d77f2d25ba3cfd1537e145bd66fddbbe19cad33f
andr3w <akeir@pm.me>
2022-10-11 22:06:02 +1100

    work in progress to install lnurl

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
 const flash = require('express-flash')
+const LnurlAuth = require('passport-lnurl-auth');
 const mongoDBSession = require('connect-mongodb-session')
 const methodOverride = require('method-override')
 const { notFoundHandler, errorHandler } = require('./middlewares/error-handlers')
 
 const User = require('./models/users')
 const Merchants = require('./models/merchants')
-const authController = require('./controllers/auth')
+// const authController = require('./controllers/auth')
 const lnMerchantController = require('./controllers/merchants')
 
 
@@ -41,9 +42,62 @@ app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
 
-passport.use(User.createStrategy())
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+// passport.use(User.createStrategy())
+// passport.serializeUser(User.serializeUser())
+// passport.deserializeUser(User.deserializeUser())
+
+
+/* ----------LNURL MIDDLEWARE---------- */
+
+passport.use(new LnurlAuth.Strategy(function(linkingPublicKey, done) {
+    const user = { id: linkingPublicKey };
+    done(null, user);
+}));
+
+app.use(passport.authenticate('lnurl-auth'));
+
+app.get('/', function(req, res) {
+    if (!req.user) {
+        return res.send('You are not authenticated. To login go <a href="/login">here</a>.');
+    }
+    res.send('Logged-in');
+});
+
+app.get('/',
+    function(req, res, next) {
+        if (req.user) {
+            return res.redirect('/');
+        }
+        next();
+    },
+    new LnurlAuth.Middleware({
+        // The externally reachable URL for the lnurl-auth middleware.
+        // It should resolve to THIS endpoint on your server.
+        callbackUrl: 'http://localhost:3000/login.html',
+        // The URL of the "Cancel" button on the login page.
+        // When set to NULL or some other falsey value, the cancel button will be hidden.
+        cancelUrl: '/',
+        // Instruction text shown below the title on the login page:
+        instruction: 'Scan the QR code to login',
+        // The file path to the login.html template:
+        loginTemplateFilePath: 'views/login.html',
+        // The number of seconds to wait before refreshing the login page:
+        refreshSeconds: 5,
+        // The title of the login page:
+        title: 'Login with lnurl-auth',
+        // The URI schema prefix used before the encoded LNURL.
+        // e.g. "lightning:" or "LIGHTNING:" or "" (empty-string)
+        uriSchemaPrefix: 'LIGHTNING:',
+        // Options object passed to QRCode.toDataURL(data, options) - for further details:
+        // https://github.com/soldair/node-qrcode/#qr-code-options
+        qrcode: {
+            errorCorrectionLevel: 'L',
+            margin: 2,
+            type: 'image/png',
+        },
+    })
+);
+/* ----------^^LNURL MIDDLEWARE---------- */
 
 app.use((req, res, next) => {
     console.log(`log from app.js ln58: ${new Date()} ${req.method} ${req.path}`);
@@ -58,7 +112,7 @@ app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
-app.use(authController)
+// app.use(authController)
 app.use(lnMerchantController)
 
 app.use(errorHandler)
