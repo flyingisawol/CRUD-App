cda901fb442b4a18b4c812af3319b9b3cd05717e
andr3w <akeir@pm.me>
2023-03-03 19:39:39 +1100

    update prior to reployment

--- controllers/merchants.js
+++ controllers/merchants.js
@@ -27,7 +27,7 @@ router.get('/merchants/search', async (req, res) => {
                     console.log(propertyArrayItem)
                     const regex = new RegExp(filters, 'i')
                     if (regex.test(propertyArrayItem)) {
-                        if (!resultsArray.includes(merchant)) {
+                        if (!resultsArray.includes(merchant[key])) {
                             resultsArray.push(merchant)
                         }
                     }
@@ -35,7 +35,7 @@ router.get('/merchants/search', async (req, res) => {
             } else {
                 const regex = new RegExp(filters, 'i') //changes search form entry to case insensitive substring
                 if (typeof merchant[key] === 'string' && regex.test(merchant[key])) { // tests if true and add to arry
-                    if (!resultsArray.includes(merchant)) {
+                    if (!resultsArray.includes(merchant[key])) {
                         resultsArray.push(merchant)
                     }
 
